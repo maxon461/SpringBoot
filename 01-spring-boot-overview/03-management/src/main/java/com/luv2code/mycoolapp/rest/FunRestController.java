d98388d00b508d18c0a6dcfbe2b026774f7526ef
Maxon laptop <vasmoto00@gmail.com>
2024-02-18 15:37:37 +0100

    secutity, @Value, terminal launch

---
+++ 01-spring-boot-overview/03-management/src/main/java/com/luv2code/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.mycoolapp.rest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+
+    // inject props fro : coach.name, team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+
+    @Value("${team.name}")
+    private String teamName;
+
+
+
+    //expose new endpoint for teaminfo
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k";
+    }
+}
